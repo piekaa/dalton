9e6412657ec5f57a73751b6d8106460f61ac7147
piotriwaniuk <piekoszek@gmail.com>
2019-05-06 20:13:28 +0200

    added privacy policy and ability to close ad

---
+++ Assets/CloseAdButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CloseAdButton : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        transform.parent.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/PrivacyPolicyButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PrivacyPolicyButton : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Application.OpenURL("http://piekoszek.com/legal/dalton/privacy-policy-android.html");
+    }
+}
